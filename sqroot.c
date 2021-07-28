b8424502af9437ad178e61d13c8aa3b4d601ea34
Prakhar Saikhedkar <56929997+Prakhar123s@users.noreply.github.com>
2021-07-28 07:50:20 +0530

    Add files via upload

---
+++ sqroot.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    float number,sq;
+
+    printf("Enter the number:");
+    scanf("%f",&number);
+    sq=sqrt(number);
+    printf("%f",sq);
+}
