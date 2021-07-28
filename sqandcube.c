bfd50e8a2b2b7d9029c0a054912a03aa8f94a8dd
Prakhar Saikhedkar <56929997+Prakhar123s@users.noreply.github.com>
2021-07-28 07:50:28 +0530

    Add files via upload

---
+++ sqandcube.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    int number;
+    printf("Enter the number:");
+    scanf("%d",&number);
+    printf("The square and cube of given number is %d and %d ", number*number, number*number*number);
+}
