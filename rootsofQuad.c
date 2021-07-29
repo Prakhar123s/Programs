ebf2aa9c0d9a603923fb0ac46d4f5dee5dec90fd
Prakhar Saikhedkar <56929997+Prakhar123s@users.noreply.github.com>
2021-07-29 13:43:34 +0530

    Add files via upload

---
+++ rootsofQuad.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a,b ,c;
+    printf("Enter a , b, c:");
+    scanf("%d %d %d", &a,&b,&c);
+
+    if(a==0)
+    {
+        printf("It is not Quadratic Equation");
+    }
+    else if((b*b)-(4*a*c)>0)
+    {
+
+    }
+}
