7454efbae93ab4d3014799a2ec600f459e0a22f7
Prakhar Saikhedkar <56929997+Prakhar123s@users.noreply.github.com>
2021-07-31 12:35:40 +0530

    Add files via upload

---
+++ radius.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    float radius, aor;
+    printf("Enter the radius of circle:");
+    scanf("%f",&radius);
+    aor=2*3.14*radius;
+    printf("The area of radius is :%2f",aor);
+}
