6bcb6898a2973b9ad55e99d1bfa0056d8dcf5299
Prakhar Saikhedkar <56929997+Prakhar123s@users.noreply.github.com>
2021-08-05 11:47:11 +0530

    Add files via upload

---
+++ pattern8.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int i,j;
+    for(int i=1;i<=5;i++)
+    {
+        for(j=i;j>=1;j--)
+        {
+            printf("%d",j%2);
+        }
+        printf("\n");
+    }
+}
